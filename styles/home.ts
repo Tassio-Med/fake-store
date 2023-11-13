7d063fd629159c57eb612305f421523409caf472
Tassio-Med <tassiohfmed@gmail.como>
2023-11-13 20:59:20 -0300

    feat: estilização geral adicionada

---
+++ styles/home.ts
@@ -0,0 +1,11 @@
+import styled  from 'styled-components';
+
+export const Home = styled.div`
+  background-color: #0f0f0f;
+  width: 100%;
+  height: calc(100vh - 76px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
