00898990a60ec41502570dedeca24ed6a14b2ffb
Tassio-Med <tassiohfmed@gmail.como>
2023-11-14 02:08:43 -0300

    feat: next configurado com styled-components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head"
 import Header from "@/components/Header"
 import Store from "@/components/Store"
-import Products from "@/components/Products"
 import * as H from '../../styles/home'
 
 export default function Home() {
@@ -13,10 +12,7 @@ export default function Home() {
     </Head>
 
     <Header/>
-    <Products/>
     <Store/>
-    <h3>Aprendendo Next.js
-    </h3>
    </H.Home>
   )
 }
