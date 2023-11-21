5e97cffa50855107f1c076036fff9b75cf6c6398
Tassio-Med <tassiohfmed@gmail.como>
2023-11-21 17:26:35 -0300

    feat: limite de elementos para chamar na apiadicionados

--- src/pages/services/api/api.jsx
+++ src/pages/services/api/api.jsx
@@ -1,15 +1,8 @@
 import axios from 'axios';
 
-export const apiProducts = async (page = 1, rows = 8, sortBy = 'id', orderBy = 'ASC') => {
+export const apiProducts = async (limit = 24) => {
   try {
-    const response = await axios.get('https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products', {
-      params: {
-        page,
-        rows,
-        sortBy,
-        orderBy,
-      },
-    });
+    const response = await axios.get(`https://fakestoreapi.com/products?limit=${limit}`);
     return response.data;
   } catch (error) {
     console.error('Error fetching products:', error.response?.data);
