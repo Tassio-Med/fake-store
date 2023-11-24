2987f6e0df51b3f73fbfbb83474f1cd1ba3a9476
Tassio-Med <tassiohfmed@gmail.como>
2023-11-24 12:40:27 -0300

    feat: provider adicionado ao app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from 'next/app'
+import { CartProvider } from '@/contexts';
 import '../../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <CartProvider>
       <Component {...pageProps} />
-    </>
+    </CartProvider>
   )
 }
 
