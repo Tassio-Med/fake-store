9168477a8793e08c1edd9fada4b82cf3f04b0621
Tassio-Med <tassiohfmed@gmail.como>
2023-11-28 12:42:14 -0300

    feat: verificação se há produtos no cart

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -27,20 +27,25 @@ export const Cart: React.FC<CartProps> = ({ onClose }) => {
       <h2>Seus produtos</h2>
       <button onClick={handleClose}><FaWindowClose /></button>
       <A.Container>
-        {cart.map((item: Product) => (
-          <A.Lista key={item.id}>
-            <A.Box>
-              <p>{item.title}</p>
-              <p>botão</p>
-              <p>R$ {item.price}</p>
-              <button onClick={() => handleRemoveItem(item.id)}>
-                <MdDeleteForever />
-              </button>
-            </A.Box>
-          </A.Lista>
-        ))}
+        {cart.length === 0 ? (
+          <p>Você não possui produtos no carrinho</p>
+        ) : (
+          cart.map((item: Product) => (
+            <A.Lista key={item.id}>
+              <A.Box>
+                <p>{item.title}</p>
+                <p>botão</p>
+                <p>R$ {item.price}</p>
+                <button onClick={() => handleRemoveItem(item.id)}>
+                  <MdDeleteForever />
+                </button>
+              </A.Box>
+            </A.Lista>
+          ))
+        )}
       </A.Container>
       <button>Finalizar Compra</button>
     </A.Cart>
   );
 };
+
