0d692c5e338f12a20f9007556d64f9a7d7f77731
Tassio-Med <tassiohfmed@gmail.como>
2023-11-29 18:30:48 -0300

    feat: cart estilizado

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,7 +1,8 @@
 import { Product } from '@/types/product.interface';
 import { useCartContext } from '@/contexts/useCartContext';
 import { MdDeleteForever } from "react-icons/md";
-import { FaWindowClose } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+import Image from 'next/image';
 
 import * as A from "./styles";
 
@@ -10,7 +11,7 @@ interface CartProps {
 }
 
 export const Cart: React.FC<CartProps> = ({ onClose }) => {
-  const { cart, removeFromCart } = useCartContext();
+  const { cart, removeFromCart, itemCount } = useCartContext();
 
   const handleClose = () => {
     if (onClose) {
@@ -24,8 +25,15 @@ export const Cart: React.FC<CartProps> = ({ onClose }) => {
 
   return (
     <A.Cart>
-      <h2>Seus produtos</h2>
-      <button onClick={handleClose}><FaWindowClose /></button>
+      <A.CloseBox>
+        <A.CloseBtn onClick={handleClose}>
+          <IoClose  style={{ fontSize: '25px', color: '#ffff' }}/>
+        </A.CloseBtn>
+      </A.CloseBox>
+      <A.Top>
+        <h2>Seus produtos</h2>
+        <h3>Você possui {itemCount} produto(s) no carrinho</h3>
+      </A.Top>
       <A.Container>
         {cart.length === 0 ? (
           <p>Você não possui produtos no carrinho</p>
@@ -33,18 +41,35 @@ export const Cart: React.FC<CartProps> = ({ onClose }) => {
           cart.map((item: Product) => (
             <A.Lista key={item.id}>
               <A.Box>
-                <p>{item.title}</p>
-                <p>botão</p>
-                <p>R$ {item.price}</p>
-                <button onClick={() => handleRemoveItem(item.id)}>
-                  <MdDeleteForever />
-                </button>
+                <A.Photo>
+                  <Image src={item.image} alt={item.title} width={70} height={70} />
+                </A.Photo>
+                <A.Details>
+                  <h4>
+                    {item.title.split(' ').length > 4
+                        ? item.title.split(' ').slice(0, 3).join(' ') + '...'
+                        : item.title
+                    }
+                  </h4>
+                  
+                  <A.BtnAndPrice>
+                    <A.QuantityButtons>
+                      <A.SubtractButton>-</A.SubtractButton>
+                      <input type="text" value="0" readOnly />
+                      <A.AddButton>+</A.AddButton>
+                    </A.QuantityButtons>
+                    <p>R$ {item.price}</p>
+                  </A.BtnAndPrice>
+                </A.Details>
               </A.Box>
+              <A.DeleteBtn onClick={() => handleRemoveItem(item.id)}>
+                <MdDeleteForever style={{ color: 'white', fontSize: '25px' }} />
+              </A.DeleteBtn>
             </A.Lista>
           ))
         )}
       </A.Container>
-      <button>Finalizar Compra</button>
+      <A.BuyBtn>Finalizar Compra</A.BuyBtn>
     </A.Cart>
   );
 };
--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -2,12 +2,44 @@ import styled from "styled-components";
 
 export const Cart = styled.aside`
   background-color: green;
-  z-index: 10;
-  width: 50%;
+  z-index: 50;
+  width: 50%; 
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 20px;
+  box-shadow: 0px 0px 2px 1px rgba(0,0,0,0.10);
+  background-color: #fff;
+  border-radius: 10px;
+`;
+
+export const CloseBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const CloseBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  border-style: inherit;
+  background-color: #fb8500;
+
+  &:hover{
+    background-color: #de7702;
+  }
+`;
+
+export const Top = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px 0;
 `;
 
 export const Container = styled.div`
@@ -16,20 +48,136 @@ export const Container = styled.div`
 
 `;
 export const Lista = styled.ul`
-  margin: 10px 10px;
-  background-color: blue;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  height: 100px;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+  margin: 30px 0px;
+  box-shadow: 0px 0px 2px 1px rgba(0,0,0,0.10);
+  border-radius: 10px;
+  
+  &:hover{
+    box-shadow: 0px 0px 6px 1px rgba(0,0,0,0.20);
+  }
+  `;
+
+export const DeleteBtn = styled.button`
+  border-color: transparent;
+  width: 10%;
+  height: 100%;
+  padding: 10px;
+  margin: 0;
   display: flex;
   justify-content: center;
   align-items: center;
-  list-style: none;
-  padding: 10px;
+  background-color: #fa1b1b;
+  
+  &:hover{
+    background-color: #d60202;
+  }
+
 `;
 
 export const Box = styled.li`
-  background-color: red;
-  width: 100%;
+  width: 90%;
   display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 10px;
+`;
+
+export const Photo = styled.div`
+  height: 100%;
+  width: 30%;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  font-family: Montserrat-Thin, sans-serif;
+  /* background-color: blue; */
+
+  h4{
+    margin-bottom: 15px;
+  }
+`;
+
+export const BtnAndPrice = styled.div`
+  display: flex;
+  width: 100%;
   justify-content: space-between;
   align-items: center;
+  
+  p {
+    width: 50%;
+    font-weight: 600;
+
+  }
+`;
+
+export const QuantityButtons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* background-color: green; */
+
+  button {
+    padding: 5px;
+    border-color: transparent;
+    color: #fff;
+    background-color: #fb8500;
+    font-weight: 700;
+    border-width: 1px;
+    margin: 0;
+    
+    &:hover{
+      background-color: #de7702;
+    }
+  }
+  
+  input {
+    border-color: #fb8500;
+    border-width: 1px;
+    border-style: solid;
+    text-align: center;
+    padding: 5px;
+    width: 50%;
+  }
+`;
+
+export const AddButton = styled.button`
+  border-radius: 0px 5px 5px 0px;
+`;
+
+export const SubtractButton = styled.button`
+  border-radius: 5px 0px 0px 5px;
+`;
+
+export const BuyBtn = styled.div`
+  width: 100%;
+  border-color: transparent;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  font-family: Montserrat-Regular;
+  font-weight: 700;
+  padding: 8px;
+  background-color: #1366e8;
+  
+  &:hover{
+    background-color: #0F52BA;
+  }
 `;
 
