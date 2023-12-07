36af94b04a5e9eb0d8933193c4b80076a8866c21
Tassio-Med <tassiohfmed@gmail.como>
2023-12-07 16:31:23 -0300

    feat: Header adicionado ao _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
 import { CartProvider } from '@/contexts';
 import '../../styles/globals.css';
+import Header from '@/components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CartProvider>
+       <Header />
       <Component {...pageProps} />
     </CartProvider>
   )
