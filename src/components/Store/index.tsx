42fac0e74f9d9baf299d7474e5c79a03d3222664
Tassio-Med <tassiohfmed@gmail.como>
2023-12-11 18:01:05 -0300

    feat: componente Card adicionado

--- src/components/Store/index.tsx
+++ src/components/Store/index.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-import Link from 'next/link';
 import * as S from './styles';
 import { Product } from '@/types/product.interface';
-import { FaTruck } from "react-icons/fa6";
-import { BsCartFill } from "react-icons/bs"
+import ProductCard from "../ProductCard";
 
 interface StoreProps {
   products: Product[];
@@ -24,36 +21,7 @@ export default function Store({ products }: StoreProps) {
       <S.ProductsGrid>
         {products.length > 0 ? (
           products.map((product) => (
-            <S.Card key={product.id}>
-              <S.ProductItem>
-                <S.Photo>
-                  <Image
-                    src={product.image}
-                    alt={product.title}
-                    width={150}
-                    height={150}
-                    loading="lazy"
-                  />
-                </S.Photo>
-                <S.Details>
-                  <h4>{truncateDescription(product.title, 35)}</h4>
-                  <S.Price>R$ {(Number(product.price)).toFixed(2)}</S.Price>
-                  <h5>em 5x R${(Number(product.price) / 5).toFixed(2)} sem juros</h5>
-                  {(Number(product.price)) > 50 && (
-                    <S.Frete>
-                      <FaTruck style={{ fontSize: '20px', color: 'green' }}  />
-                      <p>Frete Grátis</p>
-                    </S.Frete>
-                  )}
-                </S.Details>
-              </S.ProductItem>
-              <Link href={`/products/${product.id}`} passHref>
-                <S.BuyBtn>
-                    <BsCartFill style={{ color: 'white', fontSize: '20px' }} />
-                    <span>COMPRAR</span>
-                </S.BuyBtn>
-              </Link>
-            </S.Card>
+            <ProductCard key={product.id} product={product} />
           ))
         ) : (
           <p>Carregando produtos...</p>
