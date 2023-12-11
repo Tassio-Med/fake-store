05cfb550e824b1479cb7fa66c047cc6217b6337f
Tassio-Med <tassiohfmed@gmail.como>
2023-12-11 15:53:32 -0300

    feat: condição do cart para caso não tenha produtos no cart ajeitada

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -22,48 +22,47 @@ export const Cart: React.FC<CartProps> = ({ onClose }) => {
     removeFromCart(productId);
   };
 
-  
-
   return (
     <A.Cart>
       <A.Top>
         <h2>Seus produtos</h2>
-        <h3>Você possui {itemCount} produto(s) no carrinho</h3>
+        <h3>
+          {cart.length === 0
+            ? "Você não possui produtos no carrinho"
+            : `Você possui ${itemCount} produto(s) no carrinho`
+          }
+        </h3>
       </A.Top>
       <A.Container multipleProducts={cart.length > 2}>
-        {cart.length === 0 ? (
-          <p>Você não possui produtos no carrinho</p>
-        ) : (
-          cart.map((item: Product) => (
-            <A.Lista key={item.id}>
-              <A.Box>
-                <A.Photo>
-                  <Image src={item.image} alt={item.title} width={70} height={70} />
-                </A.Photo>
-                <A.Details>
-                  <h4>
-                    {item.title.split(' ').length > 4
-                        ? item.title.split(' ').slice(0, 3).join(' ') + '...'
-                        : item.title
-                    }
-                  </h4>
-                  
-                  <A.BtnAndPrice>
-                    <A.QuantityButtons>
-                      <A.SubtractButton onClick={() => decreaseQuantity(item.id)}>-</A.SubtractButton>
-                      <input type="text" value={item.quantity} readOnly />
-                      <A.AddButton onClick={() => increaseQuantity(item.id)}>+</A.AddButton>
-                    </A.QuantityButtons>
-                    <p>R$ {item.price * (item.quantity)}</p>
-                  </A.BtnAndPrice>
-                </A.Details>
-              </A.Box>
-              <A.DeleteBtn onClick={() => handleRemoveItem(item.id)}>
-                <MdDeleteForever style={{ color: 'white', fontSize: '25px' }} />
-              </A.DeleteBtn>
-            </A.Lista>
-          ))
-        )}
+        {cart.map((item: Product) => (
+          <A.Lista key={item.id}>
+            <A.Box>
+              <A.Photo>
+                <Image src={item.image} alt={item.title} width={70} height={70} />
+              </A.Photo>
+              <A.Details>
+                <h4>
+                  {item.title.split(' ').length > 4
+                      ? item.title.split(' ').slice(0, 3).join(' ') + '...'
+                      : item.title
+                  }
+                </h4>
+
+                <A.BtnAndPrice>
+                  <A.QuantityButtons>
+                    <A.SubtractButton onClick={() => decreaseQuantity(item.id)}>-</A.SubtractButton>
+                    <input type="text" value={item.quantity} readOnly />
+                    <A.AddButton onClick={() => increaseQuantity(item.id)}>+</A.AddButton>
+                  </A.QuantityButtons>
+                  <p>R$ {item.price * (item.quantity)}</p>
+                </A.BtnAndPrice>
+              </A.Details>
+            </A.Box>
+            <A.DeleteBtn onClick={() => handleRemoveItem(item.id)}>
+              <MdDeleteForever style={{ color: 'white', fontSize: '25px' }} />
+            </A.DeleteBtn>
+          </A.Lista>
+        ))}
       </A.Container>
       <A.PriceAndBtn>
         <A.Total>
@@ -75,4 +74,3 @@ export const Cart: React.FC<CartProps> = ({ onClose }) => {
     </A.Cart>
   );
 };
-
