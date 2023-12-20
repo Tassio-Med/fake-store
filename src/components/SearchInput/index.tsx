1df3bc1724a09605197c08b7da52aade0fa2538e
Tassio-Med <tassiohfmed@gmail.como>
2023-12-20 17:29:56 -0300

    feat: campo de pesquisa revisado e alterada para pesquisa por sugestão

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,28 +1,71 @@
-// SearchInput.tsx
-
-import { useState } from 'react';
-import { apiProducts } from '@/pages/services/api/api';
-import { Product } from '@/types/product.interface';
-import * as S from './styles';
+import { useState, useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
 import { IoSearch } from "react-icons/io5";
+import { apiProducts } from '@/pages/services/api/api';
+import * as S from "./styles";
+import { Product } from '@/types/product.interface';
 
-interface SearchInputProps {
-  onSearchResults: (products: Product[]) => void;
-}
-
-const SearchInput: React.FC<SearchInputProps> = ({ onSearchResults }) => {
+const SearchInput = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [inputClicked, setInputClicked] = useState(false);
+  const router = useRouter();
+  const inputRef = useRef(null);
 
-  const handleSearch = async () => {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const fetchedProducts = await apiProducts();
+        setProducts(fetchedProducts);
+      } catch (error) {
+        console.error('Erro ao obter produtos:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  useEffect(() => {
+    const fetchSuggestions = async () => {
+      try {
+        if (searchTerm.trim() === '' || !inputClicked) {
+          setSuggestions([]);
+          return;
+        }
+
+        const filteredSuggestions = products
+          .map((product) => product.title)
+          .filter((title) => title.toLowerCase().includes(searchTerm.toLowerCase()));
+
+        setSuggestions(filteredSuggestions);
+      } catch (error) {
+        console.error('Erro ao obter sugestões de pesquisa:', error);
+      }
+    };
+
+    fetchSuggestions();
+  }, [searchTerm, inputClicked, products]);
+
+  const handleSuggestionClick = async (suggestion: string) => {
+    setSearchTerm(suggestion);
+    setInputClicked(false); // Reset inputClicked state
+    const productId = getProductIdByTitle(suggestion);
     try {
-      const products = await apiProducts(); 
-      const filteredProducts = products.filter((product) =>
-        product.title.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-      onSearchResults(filteredProducts);
+      router.push(`/products/${productId}`);
     } catch (error) {
-      console.error('Erro ao buscar produtos:', error);
+      console.error('Produto não encontrado');
     }
+  };
+
+  const getProductIdByTitle = (title: string) => {
+    const product = products.find((p) => p.title === title);
+    return product ? String(product.id) : '';
+  };
+
+  const handleInputClick = () => {
+    setInputClicked(true);
+    setSuggestions([]); 
   };
 
   return (
@@ -32,9 +75,20 @@ const SearchInput: React.FC<SearchInputProps> = ({ onSearchResults }) => {
         placeholder="Pesquisar produtos"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
+        onClick={handleInputClick}
+        ref={inputRef}
       />
-      <button onClick={handleSearch}>
-        <IoSearch style={{ color: '#fff', fontSize: '20px' }}/>
+      {searchTerm && suggestions.length > 0 && (
+        <S.List multipleSuggestions={suggestions.length > 4}>
+          {suggestions.map((suggestion, index) => (
+            <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
+              {suggestion}
+            </li>
+          ))}
+        </S.List>
+      )}
+      <button>
+        <IoSearch style={{ color: '#fb8500', fontSize: '20px' }} />
       </button>
     </S.SearchInputContainer>
   );
--- src/components/SearchInput/styles.ts
+++ src/components/SearchInput/styles.ts
@@ -27,18 +27,57 @@ export const SearchInputContainer = styled.div`
   }
 
   button {
-    background-color:#fb8500;
-    padding: 5px;
-    width: 10%;
+    background-color:#fff;
+    width: 8%;
     height: 100%;
     border-radius: 0px 10px 10px 0px;
-    border: none;
+    border: solid transparent;
+  }
+`;
 
-    transition: background-color 0.4s ease;
+export const List = styled.ul<{ multipleSuggestions: boolean }>`
+  background-color: #fff;
+  list-style: none;
+  border-radius: 10px;
+  padding: 5px;
+  font-family: 'Montserrat-Regular',sans-serif;
+  font-size: 15px;
+  position: absolute; 
+  top: 70px;
+  left: 372px;
+  width: 50%; 
+  box-shadow: 0px 0px 2px 1px rgba(0,0,0,0.10);
+  height: ${(props) => (props.multipleSuggestions ? '270px' : 'auto')};
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 10px; 
+    border-radius: 10px; 
+  }
+  
+  &::-webkit-scrollbar-track {
+    background: #EBEBEB; 
+    border-radius: 10px; 
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #fb8500; 
+    border-radius: 5px; 
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: #de7702; 
+  }
+  
+  li {
+    line-height: 25px;
+    border-radius: 5px;
+    margin: 3px;
+    padding: 3px;
 
     &:hover{
-      background-color: #de7702;
+      background-color: #E6E6E6;
     }
-
   }
+
 `;
