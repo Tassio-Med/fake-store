974a79f543839ff0b4cd3822a837db5463bf65c5
Tassio-Med <tassiohfmed@gmail.como>
2023-12-20 11:06:05 -0300

    feat: componente Footer criado

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+import * as F from './styles';
+
+const Footer = () => {
+  return (
+    <F.Copy>Fake Store &copy; Todos os direitos reservados</F.Copy>
+  );
+}
+export default Footer;
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,11 @@
+import styled  from 'styled-components';
+
+export const Copy = styled.h4`
+  text-align: center;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 14px;
+  margin: 30px 0 20px 0;
+  font-family: 'Montserrat-Thin', sans-serif;
+  width: 100%;
+`;
